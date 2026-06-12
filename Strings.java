8fb228f1bb389c7e82e679d472fe2ac0628ec23e
Aakriti <bomzanaakriti@gmail.com>
2026-06-12 23:25:36 +0545

    Uppercasing using StringBuilder and largestString using lexicographical approach

--- Strings.java
+++ Strings.java
@@ -41,14 +41,41 @@ public class Strings {
         return substring;
     }
 
-    public static void main(String args[]){
-        // Scanner sc = new Scanner(System.in);
-        // String name;
-        // name = sc.nextLine();
-        
-        String path = "WEENWSSE";
+     public static String largestString (String sn[]){
+       String largest = sn[0];
+       for(int i=0;i<sn.length;i++){
+        if(largest.compareTo(sn[i])<0){
+            largest = sn[i];
+        }
+       }
+        return largest;
+     }
 
-       System.out.print(substring(path, 0, 5));
+       public static String toUpperCase (String str){
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for(int i=1;i< str.length();i++){
+          if(str.charAt(i)==' ' && i<str.length()-1){
+            sb.append(str.charAt(i));
+            i++;
+            sb.append(Character.toUpperCase(str.charAt(i)));
+          }else{
+            sb.append(str.charAt(i));
+          }
+        }
+        return sb.toString();
+
+     }
+
+    public static void main(String args[]){
+        String path = "WEENWSSE";
+        String snacks[] = {"apple","kurkure", "momo", "avocado"};
+       
+        String str = "hello, i'm aakriti";
+        System.out.println(toUpperCase(str));
+     
+
 
     }
 }
