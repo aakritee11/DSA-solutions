3fbb79ff3817f89a39e0dabc7f3a4eb18d2a15c8
Aakriti <bomzanaakriti@gmail.com>
2026-06-14 21:54:50 +0545

    Anagram strings solution

--- Strings.java
+++ Strings.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Strings {
@@ -97,12 +98,38 @@ public class Strings {
         return count;
      }
 
+      public static void isAnagrams(String str1, String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if(str1.length() == str2.length()){
+            char [] str1charArray = str1.toCharArray();
+            char [] str2charArray = str2.toCharArray();
+
+            Arrays.sort(str1charArray);
+            Arrays.sort(str2charArray);
+
+            boolean result = Arrays.equals(str1charArray, str2charArray);
+            if(result){
+                System.out.println(str1+" and "+ str2+" are anagrams");
+            }else{
+                System.out.println(str1+" and "+ str2+" are not anagrams");
+            }
+
+        }
+        else{
+            System.out.println(str1+" and "+ str2+" are not anagrams");
+        }
+
+     }
+
     public static void main(String args[]){
         String path = "WEENWSSE";
         String snacks[] = {"apple","kurkure", "momo", "avocado"};
        
-        String str = "hello, i'm aakriti";
-        System.out.println(vowelCount(str));
+        String str1 = "heart";
+        String str2 = "earthy";
+        isAnagrams(str1, str2);
      
 
 
